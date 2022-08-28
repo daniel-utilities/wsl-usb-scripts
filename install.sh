e0a99a7111fbd54158621b25f6eb861ade42ec2e
Daniel Kennedy <daniel.kennedy.m1@gmail.com>
2022-08-28 00:10:09 -0700

    Reorganization

--- install.sh
+++ install.sh
@@ -7,8 +7,17 @@
 #        CONFIGURATION VARIABLES
 # ********************************************
 
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+INSTALL_DIR="/usr/local/bin"
 
-
+INSTALL_FILES="
+$SCRIPT_DIR/usbip-attach.sh           : $INSTALL_DIR/usbip-attach
+$SCRIPT_DIR/usbip-detach.sh           : $INSTALL_DIR/usbip-detach
+$SCRIPT_DIR/usbip-list.sh             : $INSTALL_DIR/usbip-list
+$SCRIPT_DIR/usbip-automount-daemon.sh : /usr/sbin/usbip-automount
+$SCRIPT_DIR/usbip-automount-init.sh   : /etc/init.d/usbip-automount
+$SCRIPT_DIR/usbip-automount-config    : /etc/default/usbip-automount
+"
 
 
 # ********************************************
@@ -19,44 +28,67 @@
 # Inputs:
 #   None
 # Outputs:
-#   $IS_WSL2     - (true/false). If script is run within a WSL2 environment.
+#   echo true/false
 #
 is_wsl2() {
-    grep -q "WSL2" /proc/version && IS_WSL2=true || IS_WSL2=false
+    grep -q "WSL2" /proc/version && echo true || echo false
 }
 
+# trim:  Trims leading and trailing whitespace from a string
+# Inputs:
+#   trim "\t string_with_whitespace \n  "
+# Outputs:
+#   echo string_without_whitespace
+#
+trim() {
+    local var="$*"
+    var="${var#"${var%%[![:space:]]*}"}"
+    var="${var%"${var##*[![:space:]]}"}"
+    printf '%s' "$var"
+}
+
+# auto_install:  Copies files from SOURCE to DESTINATION and applies chmod +x to scripts.
+# Inputs:
+#   auto_install "SOURCE_1 : DEST_1     Copies files, creating new folders if necessary.
+#                 SOURCE_2 : DEST_2 "   Will use 'sudo' if necessary to gain write privilege.
+# Outputs:
+#   None
+#
+auto_install() {
+    IFS=$'\n' declare -a 'ARR=($*)'
+    for LINE in "${ARR[@]}"; do
+        IFS=':' declare -a 'PAIR=($LINE)'
+        local SRC=$(trim "${PAIR[0]}")
+        local DST=$(trim "${PAIR[1]}")
+        local DST_DIR=$(dirname "$DST")
+        echo "$SRC --> $DST"
+        mkdir -p "$DST_DIR" 2> /dev/null || sudo mkdir -p "$DST_DIR"
+        cp -f "$SRC" "$DST" 2> /dev/null || sudo cp -f "$SRC" "$DST"
+        if [[ "$SRC" == *.sh ]]; then 
+            chmod +x "$DST" 2> /dev/null || sudo chmod +x "$DST"
+        fi
+    done
+}
+# printf "%s" "$IFS" | od -bc
 
 # ********************************************
 #                BEGIN
 # ********************************************
 
 # Assert WSL2
-is_wsl2
-if [[ "$IS_WSL2" == false ]]; then
+if [[ "$(is_wsl2)" == false ]]; then
     echo "This script must be run inside WSL2 only!"
     exit
 fi
 
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-INSTALL_DIR="/usr/local/bin"
 
 # Install USB-IP drivers in WSL2
 sudo apt-get install -y linux-tools-virtual hwdata
 sudo update-alternatives --install /usr/local/bin/usbip usbip `ls /usr/lib/linux-tools/*/usbip | tail -n1` 20   
 
-# Copy scripts to INSTALL_DIR
-cd "$SCRIPT_DIR"
-sudo cp -f "usbip-attach.sh"  "$INSTALL_DIR/usbip-attach" && sudo chmod +x "$INSTALL_DIR/usbip-attach"
-sudo cp -f "usbip-detach.sh"  "$INSTALL_DIR/usbip-detach" && sudo chmod +x "$INSTALL_DIR/usbip-detach"
-sudo cp -f "usbip-list.sh"    "$INSTALL_DIR/usbip-list"   && sudo chmod +x "$INSTALL_DIR/usbip-list"
-#sudo cp -f "usbip-attach.bat" "$INSTALL_DIR/usbip-attach.bat"
-
-
-# Install usbip-automount as an init.d service
+# Install new files 
 service usbip-automount stop
-sudo cp -f "usbip-automount-daemon.sh" "/usr/sbin/usbip-automount"   && sudo chmod +x "/usr/sbin/usbip-automount"
-sudo cp -f "usbip-automount-init.sh"   "/etc/init.d/usbip-automount" && sudo chmod +x "/etc/init.d/usbip-automount"
-sudo cp -f "usbip-automount-config"    "/etc/default/usbip-automount"
+auto_install "$INSTALL_FILES"
 
 # Give user permissions to start udev service
 APPEND="$USER ALL=(ALL) NOPASSWD: /etc/init.d/udev"
